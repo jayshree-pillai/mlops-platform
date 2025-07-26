6754dce49131f3fb6644a2d1b60a20f106bc023e
Your Name <jayshree.pillai@gmail.com>
2025-07-26 10:33:20 -0400

    upgrade tf version to fix account_id issue

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,10 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 5.32.0"
+      version = "4.67.0"
     }
   }
-
   required_version = ">= 1.3.0"
 }
 
