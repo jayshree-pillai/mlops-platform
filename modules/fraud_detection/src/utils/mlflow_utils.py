506126ec923465810a97a649b180867dfb37fba9
Your Name <jayshree.pillai@gmail.com>
2025-08-03 19:42:13 -0400

    fixed the mlflow issues

--- modules/fraud_detection/src/utils/mlflow_utils.py
+++ modules/fraud_detection/src/utils/mlflow_utils.py
@@ -1,7 +1,7 @@
 import mlflow
 
-def init_mlflow(tracking_uri="http://3.215.110.164:5000", experiment_name="fraud_default"):
+def init_mlflow(tracking_uri="http://127.0.0.1:5000", experiment_name="fraud_default"):
     mlflow.set_tracking_uri(tracking_uri)
     mlflow.set_experiment(experiment_name)
     print("🔗 Tracking URI:", mlflow.get_tracking_uri())
-    #print("📁 Experiment:", mlflow.get_experiment_by_name(experiment_name))
+    print("📁 Experiment:", mlflow.get_experiment_by_name(experiment_name))
